cb60c909fe58ac21f3e73a963bb7d178f9e4d96e
fluffymormegil <mpread@chiark.greenend.org.uk>
2011-04-26 22:26:30 +0100

    Rearranged stlfgets headerage

--- include/libmormegil/stlwfgets.hh
+++ include/libmormegil/stlwfgets.hh
@@ -1,4 +1,4 @@
-// libmormegil/stlfgets.hh
+// libmormegil/stlwfgets.hh
 //
 // In jurisdictions where this file would be adjuged to contain copyrightable
 // material, it is copyright 2011 Martin Read, and released to the public
@@ -10,7 +10,7 @@
 
 namespace libmormegil
 {
-    int stlfgets(std::string& str, FILE *fp, size_t size_suggest = 128, size_t hard_size = 0x7fffffff);
+    int stlwfgets(std::wstring& str, FILE *fp, size_t size_suggest = 128, size_t hard_size = 0x7fffffff);
 }
 
 // vim:ts=8:sw=4:expandtab:fo=croq
